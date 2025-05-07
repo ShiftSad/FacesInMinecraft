921e5b8601e5040279400f42f727eefc78b209bd
Sadness <rosalimavictor@gmail.com>
2025-05-07 01:10:41 -0300

    First commit v2

---
+++ server/src/main/kotlin/dev/shiftsad/faceMod/FaceMod.kt
@@ -0,0 +1,14 @@
+package dev.shiftsad.faceMod
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FaceMod : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
